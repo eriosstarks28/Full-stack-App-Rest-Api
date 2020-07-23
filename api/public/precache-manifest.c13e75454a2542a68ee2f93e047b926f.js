ac8598f1d49220e0b4b5424e83a3bead4e8d22f0
Elias Starks <58051709+eriosstarks28@users.noreply.github.com>
2020-07-23 17:47:26 -0400

    mongo ready

---
+++ api/public/precache-manifest.c13e75454a2542a68ee2f93e047b926f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a77418e946a489fe1e7fc5bf2a0415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cac69a4599f561281551",
+    "url": "/static/css/main.c3f71c8f.chunk.css"
+  },
+  {
+    "revision": "115ac46b88819160b72b",
+    "url": "/static/js/2.d6198068.chunk.js"
+  },
+  {
+    "revision": "de81bfc3d566733bec20fcc6cadc32a6",
+    "url": "/static/js/2.d6198068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac69a4599f561281551",
+    "url": "/static/js/main.a369ba2d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
